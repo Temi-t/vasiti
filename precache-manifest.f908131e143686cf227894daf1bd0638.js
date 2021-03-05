572a32500677970415c834c72319b8697c50a560
Temi-t <temiemmanuel@yahoo.com>
2021-03-05 18:48:44 +0100

    Updates

---
+++ precache-manifest.f908131e143686cf227894daf1bd0638.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8140aae2caff64816d052154920f42eb",
+    "url": "/vasiti/index.html"
+  },
+  {
+    "revision": "c0d752d08f2e4e6b4320",
+    "url": "/vasiti/static/css/main.348a6590.chunk.css"
+  },
+  {
+    "revision": "2492501d68c9507f1acf",
+    "url": "/vasiti/static/js/2.9b6114cc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vasiti/static/js/2.9b6114cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d752d08f2e4e6b4320",
+    "url": "/vasiti/static/js/main.12cab874.chunk.js"
+  },
+  {
+    "revision": "b34825b2c6d0ce76f903",
+    "url": "/vasiti/static/js/runtime-main.0e43d44b.js"
+  },
+  {
+    "revision": "2f047f457efb30ed068c8dd379602c9d",
+    "url": "/vasiti/static/media/Adetola.2f047f45.png"
+  },
+  {
+    "revision": "a85cd5020f43c1a0d998e49c8599d8c5",
+    "url": "/vasiti/static/media/good-guy.a85cd502.png"
+  },
+  {
+    "revision": "b9c48fbdf90f8dc5142714b5e3880c59",
+    "url": "/vasiti/static/media/phone.b9c48fbd.png"
+  },
+  {
+    "revision": "fd7e44e03869205321d31216e6358a73",
+    "url": "/vasiti/static/media/smiling-couples.fd7e44e0.png"
+  },
+  {
+    "revision": "256dbd9bcaad107ba2fa76e5734224b4",
+    "url": "/vasiti/static/media/smiling-ladies.256dbd9b.png"
+  },
+  {
+    "revision": "db02f9b57d355c42a89ff9803c6232da",
+    "url": "/vasiti/static/media/vasiti.db02f9b5.png"
+  },
+  {
+    "revision": "b5c493da09dafeb8f6ca13cd7bf2b727",
+    "url": "/vasiti/static/media/woman-shoppingbag-card 1.b5c493da.png"
+  }
+]);
