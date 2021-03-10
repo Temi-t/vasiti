766c7645541360a15883677a5bc5400565cc0282
Temi-t <temiemmanuel@yahoo.com>
2021-03-10 22:18:20 +0100

    technical assessment

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,12 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Story from './Story';
 // import { useStoryContext, useHandleStoryContext } from './StoryContext';
 import { useStoryContext } from './StoryContext';
 
+const LS_KEY = "story.key";
 export default function Stories () {
-      const storyInput = useRef();
+    const storyInput = useRef();
     const [stories, setStories] = useStoryContext();
     // const handleStory = useHandleStoryContext();
+
+    useEffect(()=>{
+      const storedStories = JSON.parse(localStorage.getItem(LS_KEY))
+      storedStories && setStories(storedStories)
+    }, []);
+    useEffect(()=>{
+      localStorage.setItem(LS_KEY, JSON.stringify(stories))
+    }, [stories]);
 
   const rand = () => {
     return Math.floor((Math.random() * 10000) + 1)
@@ -22,19 +31,35 @@ export default function Stories () {
     ])
     storyInput.current.value = null
   }
+
+  const clearStory = () => {
+    localStorage.clear()
+  }
   
     return(
         <div>
-            <div className="story-class">
+            <form className="story-class" onSubmit={handleStory}>
+              <h2>Share your amazing story!</h2>
               <label htmlFor="upload">
-                Upload picture
+                Upload your picture
               </label>
-              <input name="upload" type= "file" placeholder="Choose Image"/>
+              <input id="upload" name="upload" type= "file" placeholder="Choose Image"/>
 
+              {/* nameInput and lastNameInput */}
+              <input type="text" name="firstName" placeholder="Enter first name" /> 
+              <input type="text" name="lastName" placeholder="Enter last name" /> 
+              {/* textField */}
+              <input type="textfield" row="5" column="9"/>
+              {/* what did you 
+              interact with vasiti as? 
+              customer/vendor radio button */}
+
+              {/* Share your story! */}
 
               <input type="text" ref={storyInput}/>
-              <button onClick={handleStory}>Share your story!</button>
-            </div>
+              <button type="submit">Share your story!</button>
+              <button onClick={clearStory}>Delete my Stories</button>
+            </form>
             {stories.map((story, i)=> <Story story={story} key={"story_"+ i} />)}
             
         </div>
